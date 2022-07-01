5a80c9aae558601622dc0d27f1ec1adc818d3506
hercules-kan <carloselara034@gmail.com>
2022-07-01 15:16:47 -0500

    "Valores (Claro que todo puede mejorar) a parte los hice en modo system para hacer la cosita un poquito más compleja. Estoy agradecido y feliz de poder ser parte de open bootcamp"

--- src/com/paquete/Main.java
+++ src/com/paquete/Main.java
@@ -1,7 +1,35 @@
 package com.paquete;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("\n----------------------------------------------------------------\n");
+        System.out.println("Ingreso de diferentes tipos de datos");
+        System.out.println("\n----------------------------------------------------------------\n");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce un String");
+        String input = scanner.nextLine();
+        System.out.println("Introduce un long ");
+        long largo = scanner.nextLong();
+        System.out.println("Introduce un double ");
+        double val = scanner.nextDouble();
+        System.out.println("Introduce un float ");
+        float fval = scanner.nextFloat();
+        System.out.println("Introduce un int ");
+        int i = scanner.nextInt();
+        System.out.println("Introduce un boolean ");
+        boolean b = scanner.nextBoolean();
+        System.out.println("Introduce un byte ");
+        byte bbyte = scanner.nextByte();
+        System.out.println("Introduce un short ");
+        short s = scanner.nextShort();
+        System.out.println("Introduce un char ");
+        char c = scanner.next().charAt(0);
+        System.out.println("\n Usted introdujo los siguientes valorese "+" "+ input+" " +" "+ largo+" " + val+" " + fval+" " + i+" " + b+" " + bbyte+" " + s+" " + c);
+
+
+
     }
 }
